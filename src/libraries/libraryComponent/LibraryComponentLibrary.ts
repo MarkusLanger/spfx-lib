3acd27ecbe01b5b52d3cd17e74e6e591aa6bd56d
Markus Langer <markus.langer@gis-ag.com>
2021-05-05 14:32:17 +0200

    import and use strings

--- src/libraries/libraryComponent/LibraryComponentLibrary.ts
+++ src/libraries/libraryComponent/LibraryComponentLibrary.ts
@@ -1,5 +1,7 @@
+//import * as strings from 'LibraryComponentLibraryStrings';
 export class LibraryComponentLibrary {
   public name(): string {
     return 'LibraryComponentLibrary';
+     //return 'LibraryComponentLibrary' +  ' => ' + strings.BasicGroupName;
   }
 }
